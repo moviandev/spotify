69a78acc65176c0f92e5a316abdc15ea7f68afb8
Matheus Viana <matheus_o_viana@hotmail.com>
2019-11-19 18:40:44 -0300

    chore: fixed instructions in config.env and fixed server.js

--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
-
-const DB_LOCAL = process.env.DATABASE_LOCAL;
-
-console.log(DB_LOCAL);
-
 process.on('uncaughtException', err => {
   global.console.log('SHUTTING DOWN APP UNCAUGHT EXCEPTION');
   global.console.log(err.name, err.message);
   process.exit(1);
 });
+
+dotenv.config({ path: './config.env' });
+
+const DB_LOCAL = process.env.DATABASE_LOCAL;
+
+const DB = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
 
 mongoose.createConnection(DB_LOCAL);
 
